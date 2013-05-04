eaebf949ce3be0186b12f8e475f14dbed5b02f0e
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-05-04 17:11:20 +1000

    20130504 using official headset button fix. test disabling baseband_xmm_power wake lock in kernel

--- do_patch.sh
+++ do_patch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ux
 PATCH_DIR='../arararagi_lge_p880_patches/patches'
 
-#9168e64ff9b4d2ae2ff8e21356a9063314de41c5
+#54148870b37e288b3cdf7830026adf67c2e00254
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.cached.patch"
 
-#9168e64ff9b4d2ae2ff8e21356a9063314de41c5
+#54148870b37e288b3cdf7830026adf67c2e00254
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.patch"
 
 #26caff32fde6413ce7f14a543d4300bc252af6ae
@@ -16,7 +16,7 @@ git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.ca
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
 git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.patch"
 
-#e0d6d0f1baea1a7c9394ead26859a13802b4434a
+#7fc7e995af38aa746b4d9a8434830569136390bb
 git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.patch"
 
 #9ff25ee7280b1107f08e5a0eb03b050ce71d797e
