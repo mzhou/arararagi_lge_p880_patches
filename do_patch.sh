37c4fb882989873fd031a6e6930653b5b397addb
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-05-12 23:14:11 +1000

    20130512 Make torch brightness adjustable

--- do_patch.sh
+++ do_patch.sh
@@ -7,7 +7,7 @@ git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.cached.patch
 #54148870b37e288b3cdf7830026adf67c2e00254
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.patch"
 
-#b411b98b1e7db62b40c641eaf22731eb555f2fa4
+#a33a12bd8b492daeca0ea8bc48c2b8ed28179519
 git apply --directory='frameworks/base' "$PATCH_DIR/frameworks-base.patch"
 
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
@@ -22,6 +22,9 @@ git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.cached.patch
 #4bec6920dba5d4eea4e827d06ea152ea541aa9fc
 git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.patch"
 
-#1417786caa95bc7db3da5ab060c5d1c6cd3aa0cc
+#cc41032d68756851eb2d01457cabbc3153f7423f
 git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.patch"
+
+#2077ef145290eb9ecdf1eefd2b2414637a64de31
+git apply --directory='packages/apps/Torch' "$PATCH_DIR/packages-apps-Torch.patch"
 
