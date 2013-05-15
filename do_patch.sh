5a646d48b06ba46db50c2b89d54165bbafd24d46
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-05-16 00:30:47 +1000

    20130516 Torch fixes. Disable touch event debugging print in kernel.

--- do_patch.sh
+++ do_patch.sh
@@ -7,7 +7,7 @@ git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.cached.patch
 #54148870b37e288b3cdf7830026adf67c2e00254
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.patch"
 
-#a33a12bd8b492daeca0ea8bc48c2b8ed28179519
+#cdd98977f5089eb829724579943dbe23480ea272
 git apply --directory='frameworks/base' "$PATCH_DIR/frameworks-base.patch"
 
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
@@ -22,7 +22,7 @@ git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.cached.patch
 #4bec6920dba5d4eea4e827d06ea152ea541aa9fc
 git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.patch"
 
-#cc41032d68756851eb2d01457cabbc3153f7423f
+#ba7f45a393bbdbcc3e94aa340212e595230bb90b
 git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.patch"
 
 #2077ef145290eb9ecdf1eefd2b2414637a64de31
