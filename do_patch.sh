be398d8681fce1cbb6ea70e988fc316b97da52bc
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-05-19 23:11:58 +1000

    20130519

--- do_patch.sh
+++ do_patch.sh
@@ -7,7 +7,7 @@ git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.cached.patch
 #54148870b37e288b3cdf7830026adf67c2e00254
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.patch"
 
-#cdd98977f5089eb829724579943dbe23480ea272
+#4641ba204604e29befa5d87e0fced5bb9221c380
 git apply --directory='frameworks/base' "$PATCH_DIR/frameworks-base.patch"
 
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
@@ -22,7 +22,7 @@ git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.cached.patch
 #4bec6920dba5d4eea4e827d06ea152ea541aa9fc
 git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.patch"
 
-#ba7f45a393bbdbcc3e94aa340212e595230bb90b
+#b27c3ef9bd8c0bdf7ebdfd12324ea9e9421f5aee
 git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.patch"
 
 #2077ef145290eb9ecdf1eefd2b2414637a64de31
