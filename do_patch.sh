ed84e516853432a2c665f3b9fafc8f6fa4c1dc9d
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-06-09 23:13:01 +1000

    20130609: add debugging to baseband_xmm_power. uncap backlight brightness

--- do_patch.sh
+++ do_patch.sh
@@ -7,7 +7,7 @@ git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.cached.patch
 #54148870b37e288b3cdf7830026adf67c2e00254
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.patch"
 
-#d4a07e7d0e3b2f0fad31884a8e838521ab2fb98c
+#558a3c671dc2cfc520ea9a515ce9ecba5c789b18
 git apply --directory='frameworks/base' "$PATCH_DIR/frameworks-base.patch"
 
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
@@ -19,9 +19,9 @@ git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.pa
 #4bec6920dba5d4eea4e827d06ea152ea541aa9fc
 git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.patch"
 
-#ff42ec782a25e92135c0a362644c58b9ca0c210f
-git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.cached.patch"
+#6752d08e59b3b27b96ffc9c4f5ebe8c36b03b3ea
+git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.patch"
 
-#5628d44466c9d7772db96c2f6ae0e45cb2c2c0d0
+#ce44db57d29a01a5c2f9e86aace2127d92b95c73
 git apply --directory='packages/apps/Torch' "$PATCH_DIR/packages-apps-Torch.patch"
 
