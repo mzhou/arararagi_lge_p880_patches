dd236fb59c666c6decbec4f45a217ae2a436591f
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-06-14 11:46:04 +1000

    20130613.1: OpenPDroid

--- do_patch.sh
+++ do_patch.sh
@@ -7,6 +7,9 @@ git apply --directory='bionic' "$PATCH_DIR/bionic.cached.patch"
 #c9a8d3d5c6a9363590372fa73ac43273817af43b
 git apply --directory='bionic' "$PATCH_DIR/bionic.patch"
 
+#7da16d025b2c972200ffb5b308fe1683e62e564a
+git apply --directory='build' "$PATCH_DIR/build.cached.patch"
+
 #54148870b37e288b3cdf7830026adf67c2e00254
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.cached.patch"
 
@@ -31,6 +34,9 @@ git apply --directory='frameworks/base' "$PATCH_DIR/frameworks-base.cached.patch
 #452bb346368d84742ceed62cb9ecba17c2c1e053
 git apply --directory='frameworks/rs' "$PATCH_DIR/frameworks-rs.patch"
 
+#aee7b8203fae90edbc91ed879ffb6e53bb90b2d2
+git apply --directory='frameworks/opt/telephony' "$PATCH_DIR/frameworks-opt-telephony.cached.patch"
+
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
 git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.cached.patch"
 
@@ -39,6 +45,12 @@ git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.pa
 
 #c884ed736e71eb2b0c545517cddb1d75a03172e4
 git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.patch"
+
+#8a22bed790b6699fc5e52c7d796b556ef92ecb9c
+git apply --directory='libcore' "$PATCH_DIR/libcore.cached.patch"
+
+#3aca6c3137f968f4c4103e15f0fbbe0141dd518e
+git apply --directory='packages/apps/Mms' "$PATCH_DIR/packages-apps-Mms.cached.patch"
 
 #7dae958adefd8f4a7594a74f8e05f1e3449e6cc6
 git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.patch"
