537f97dcc5da659b15c1f00eb556055e7be87712
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-06-27 21:34:41 +1000

    20130626: Fix saving HALO settings. Apply PA fixes to HALO.
    commit 11bbf6cf5e5f4c45bc8a80c34822883e7ee4e4d1
    Author: beerbong <yghazi@gmail.com>
    Date:   Wed Jun 19 13:25:31 2013 +0200
    
        Give floating flag to previous activities in the stack
        When launching stacked activities, only the last one is flagged as floating in sysui when tapping halo.
        This causes the system to show the launcher when the first stacked activity is launched (this first intent gets the flag FLAG_ACTIVITY_TASK_ON_HOME
        in TaskStackBuilder).
        This change makes all the stacked activities get the floating flag if the last one has it.
    
    commit bf3b228972196407c33113ed88f32d67127c2c64
    Author: Jesús David <jedga_95@outlook.com>
    Date:   Sat Jun 15 14:43:23 2013 -0500
    
        Show as floating window on recent apps
    
        This also removes "hybrid properties" from recents menu, it is fairly unneeded
        now. This is a test, It's probable that it will be removed later
    
        Change-Id: I3db3a996fa08367e3dfa4231079fe5e811e3b08a
    
    commit 8c7024cef33d598cf57437ede0ed11e13f4371bf
    Author: Jesús David <jedga_95@outlook.com>
    Date:   Fri Jun 14 11:16:09 2013 -0500
    
        Fix little derp causing visual bug
    
        To reproduce, disable halo, and press "clear all notifications" button
    
        Change-Id: Ice7ab62229e27db53ef51fd5df55dbcd0a86358b
    
    commit d12f8ee8c89f62e95e95d2d245d42af5e3ebb669
    Author: beerbong <yghazi@gmail.com>
    Date:   Fri Jun 14 07:38:56 2013 +0200
    
        Reworked transparent layer
        When a halo'ed window is opened, the transparent layer will be always visible. It will manage the background dim.
        This fixes some apps dimming its sandboxed processes (like chrome).
    
        LayerActivity manages its own lifecycle. When it's focused after paused it finishes itself.
    
        System processes (identified by "android" pkg in SystemUI and system uid in Activity) don't use the layer and still use their own background. This
        has been kept this way to not interfere with their activity.

--- do_patch.sh
+++ do_patch.sh
@@ -13,10 +13,10 @@ git apply --directory='build' "$PATCH_DIR/build.cached.patch"
 #ba2d49d4cdb95e6508d775f3ff148579318a1051
 git apply --directory='build' "$PATCH_DIR/build.patch"
 
-#54148870b37e288b3cdf7830026adf67c2e00254
+#84886c0b05d26690854da772e916fcef787ddaca
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.cached.patch"
 
-#54148870b37e288b3cdf7830026adf67c2e00254
+#84886c0b05d26690854da772e916fcef787ddaca
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.patch"
 
 #e36e25b6032bb8d194792e77d78f212997b40713
@@ -31,7 +31,7 @@ git apply --directory='external/clang' "$PATCH_DIR/external-clang.patch"
 #eaf99024ae65895f4d843fbdd46702432d65d821
 git apply --directory='external/v8' "$PATCH_DIR/external-v8.patch"
 
-#faf556956cb8d33ef496aff0a897cf26c7ade6e1
+#1621c284856753f674a0f54ec6f0f7c71a3a98ba
 git apply --directory='frameworks/base' "$PATCH_DIR/frameworks-base.cached.patch"
 
 #452bb346368d84742ceed62cb9ecba17c2c1e053
@@ -55,10 +55,10 @@ git apply --directory='libcore' "$PATCH_DIR/libcore.cached.patch"
 #d5757a2a47aec7b14132b85702d441ef39edd6ca
 git apply --directory='packages/apps/Mms' "$PATCH_DIR/packages-apps-Mms.cached.patch"
 
-#41ef818e08ebb13cedf999c347ed79d75d938364
+#dabc269158d4d6ff0305a17ab63704c74acaebc1
 git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.cached.patch"
 
-#d91f217cb50f7c92f8276e12e9bba9d00af55008
+#6e5a47498bc45044d3ddb021f756038e9f8cbaca
 git apply --directory='packages/apps/Torch' "$PATCH_DIR/packages-apps-Torch.patch"
 
 #cd8df62842b81c93022feabfacda8efd257e1754
