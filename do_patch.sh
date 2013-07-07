823e80fcc3e0072d9ee57e4470ee20f931780c3c
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-07-07 19:45:43 +1000

    20130707: Add stock hidden menu (only GNSS Test working)

--- do_patch.sh
+++ do_patch.sh
@@ -7,16 +7,16 @@ git apply --directory='bionic' "$PATCH_DIR/bionic.cached.patch"
 #80fcbd05207cd2d1e3673ed9536d7a86c2fa251a
 git apply --directory='bionic' "$PATCH_DIR/bionic.patch"
 
-#767350772b93c8dad1a438b67ef04ebab9b7cafe
+#3fb440ac788d755237c083023b0d68ca3ec43869
 git apply --directory='build' "$PATCH_DIR/build.cached.patch"
 
-#767350772b93c8dad1a438b67ef04ebab9b7cafe
+#3fb440ac788d755237c083023b0d68ca3ec43869
 git apply --directory='build' "$PATCH_DIR/build.patch"
 
-#84886c0b05d26690854da772e916fcef787ddaca
+#d27de8481b776f06ee2d9e233cd470dec9e95c83
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.cached.patch"
 
-#84886c0b05d26690854da772e916fcef787ddaca
+#d27de8481b776f06ee2d9e233cd470dec9e95c83
 git apply --directory='device/lge/p880' "$PATCH_DIR/device-lge-p880.patch"
 
 #e36e25b6032bb8d194792e77d78f212997b40713
@@ -31,13 +31,13 @@ git apply --directory='external/clang' "$PATCH_DIR/external-clang.patch"
 #eaf99024ae65895f4d843fbdd46702432d65d821
 git apply --directory='external/v8' "$PATCH_DIR/external-v8.patch"
 
-#9e3fa9cb673f69aa69de0294bebba61d10befddc
+#ffda8dd5c1f569d711b47af8cfad034fb1ba02cc
 git apply --directory='frameworks/base' "$PATCH_DIR/frameworks-base.cached.patch"
 
 #452bb346368d84742ceed62cb9ecba17c2c1e053
 git apply --directory='frameworks/rs' "$PATCH_DIR/frameworks-rs.patch"
 
-#3dba6580fc385399d726233692d5abb3b5b8208b
+#57f67b4244ea8953296f6e4905286e951c3b2918
 git apply --directory='frameworks/opt/telephony' "$PATCH_DIR/frameworks-opt-telephony.cached.patch"
 
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
@@ -46,24 +46,24 @@ git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.ca
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
 git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.patch"
 
-#c884ed736e71eb2b0c545517cddb1d75a03172e4
+#267b77b7ad75dd190ff1ee7173d900d262a9a750
 git apply --directory='kernel/lge/p880' "$PATCH_DIR/kernel-lge-p880.patch"
 
 #8a22bed790b6699fc5e52c7d796b556ef92ecb9c
 git apply --directory='libcore' "$PATCH_DIR/libcore.cached.patch"
 
-#05fe8b2b748163c909ed677c0d333eb565d6f1d8
+#599dded8e0e7a7939ea362f7220bb539c9617414
 git apply --directory='packages/apps/Mms' "$PATCH_DIR/packages-apps-Mms.cached.patch"
 
-#765742df4c111ed7cad8c2b2798c748d7bd14cc5
+#0200bfffd19b19c7816c067d09fe1a3d145ffa0b
 git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.cached.patch"
 
-#9fdf522be2654e42cc374f0c85278b08c6cb8328
+#7aeb49c1c9bf37e83fb4dc919fcfa1f30c300668
 git apply --directory='packages/apps/Torch' "$PATCH_DIR/packages-apps-Torch.patch"
 
 #cd8df62842b81c93022feabfacda8efd257e1754
 git apply --directory='system/security' "$PATCH_DIR/system-security.patch"
 
-#4852f0ab4d60a74a6d858b728d94d7bb393178c2
+#4ffddabd4311a2415b5370a8652a98594ddcbb63
 git apply --directory='system/core' "$PATCH_DIR/system-core.patch"
 
