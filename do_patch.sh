506fc72638955e810782cabfc41d02a503ebadca
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-07-13 19:33:38 +1000

    20130713

--- do_patch.sh
+++ do_patch.sh
@@ -7,10 +7,10 @@ git apply --directory='bionic' "$PATCH_DIR/bionic.cached.patch"
 #80fcbd05207cd2d1e3673ed9536d7a86c2fa251a
 git apply --directory='bionic' "$PATCH_DIR/bionic.patch"
 
-#3fb440ac788d755237c083023b0d68ca3ec43869
+#e5bdddf18ce2b28d4b9ced32413f54d96840e01e
 git apply --directory='build' "$PATCH_DIR/build.cached.patch"
 
-#3fb440ac788d755237c083023b0d68ca3ec43869
+#e5bdddf18ce2b28d4b9ced32413f54d96840e01e
 git apply --directory='build' "$PATCH_DIR/build.patch"
 
 #d27de8481b776f06ee2d9e233cd470dec9e95c83
@@ -31,13 +31,13 @@ git apply --directory='external/clang' "$PATCH_DIR/external-clang.patch"
 #eaf99024ae65895f4d843fbdd46702432d65d821
 git apply --directory='external/v8' "$PATCH_DIR/external-v8.patch"
 
-#34b32f417964496d14d043368eac521159247ea7
+#d9eefe37ce5adc43043d929b8e03d4193a11949e
 git apply --directory='frameworks/base' "$PATCH_DIR/frameworks-base.cached.patch"
 
 #452bb346368d84742ceed62cb9ecba17c2c1e053
 git apply --directory='frameworks/rs' "$PATCH_DIR/frameworks-rs.patch"
 
-#b7abe4eecf521ec79c0797cfa8ea67e4505a97c4
+#6a8675a72d5f9f032c4ad9b9da4158a12306cea6
 git apply --directory='frameworks/opt/telephony' "$PATCH_DIR/frameworks-opt-telephony.cached.patch"
 
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
@@ -46,21 +46,21 @@ git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.ca
 #86fc2992b5a91da8840637f90cf4453f75e44bf0
 git apply --directory='hardware/libhardware' "$PATCH_DIR/hardware-libhardware.patch"
 
-#fe70e697810a7a8b9ce47325f53d16fdbc19f1f8
+#35d2187f72e95136574ead9323fb4fb7d4feb4dc
 git apply --directory='libcore' "$PATCH_DIR/libcore.cached.patch"
 
-#62d2a73f7172766a32d120db0c90c14931bdb6c4
+#ccdd9801cc179baf2695192d93b6fd82c7a472f7
 git apply --directory='packages/apps/Mms' "$PATCH_DIR/packages-apps-Mms.cached.patch"
 
-#5a62ab4a34c4cbc3d9cb31b81eb0495b43a61b02
+#2f644d46e58c650b9086ee2c98e46dbef39b1db0
 git apply --directory='packages/apps/Settings' "$PATCH_DIR/packages-apps-Settings.cached.patch"
 
-#597cae6164277cc6244e59ecfe04dba8b93f3d03
+#df7010c39c51aaf4b77e84e7feb8699a908f5929
 git apply --directory='packages/apps/Torch' "$PATCH_DIR/packages-apps-Torch.patch"
 
 #cd8df62842b81c93022feabfacda8efd257e1754
 git apply --directory='system/security' "$PATCH_DIR/system-security.patch"
 
-#4ffddabd4311a2415b5370a8652a98594ddcbb63
+#fee496b0ffcfc0fd1598d5381cc101b13a689200
 git apply --directory='system/core' "$PATCH_DIR/system-core.patch"
 
